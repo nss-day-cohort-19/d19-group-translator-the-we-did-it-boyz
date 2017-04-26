d94fd7f8c3547e3e43564a08d01d87238bffedd6
Jason Smith <jasonlsmith52@gmail.com>
2017-04-26 14:29:11 -0500

    added order change functionality

--- piglatin.js
+++ piglatin.js
@@ -1,27 +1,29 @@
-var pigLatin= (function(Translator){
-var pigLatinWords = [["merry","errymay"], ["christmas","hristmascay"], ["and","ndaay"], ["happy","appyhay"], ["new","ewnay"], ["year","earyay"]];
+if(typeof Translator === "undefined") {
+		var Translator = (function(){return {
+		}})();
+	}
+var PigLatin = (function(Translator){
+	var pigLatinWords = [["merry","errymay"], ["christmas","hristmascay"], ["and","ndaay"], ["happy","appyhay"], ["new","ewnay"], ["year","earyay"]];
 
-
-		Translator.translatePigLatin= function (message) {
-			var newMessage=[];
-			for(word in message) {
-				for (var i = 0; i < pigLatinWords.length; i++) {
-					console.log(message[word], pigLatinWords[i][1]);
-					if(pigLatinWords[i][0] == message[word]) {
-						newMessage.push(pigLatinWords[i][1]);
-						console.log(word);
-						break;
-					}
+	Translator.translatePigLatin = function (message) {
+		var newMessage=[];
+		for(word in message) {
+			for (var i = 0; i < pigLatinWords.length; i++) {
+				console.log(message[word], pigLatinWords[i][1]);
+				if(pigLatinWords[i][0] == message[word]) {
+					newMessage.push(pigLatinWords[i][1]);
+					console.log(word);
+					break;
 				}
 			}
-			console.log(newMessage);
-			var message2 = newMessage.reduce(function(a, b) {
-
-				return a + " " + b;
-			});
-			return message2;
 		}
+		console.log(newMessage);
+		var message2 = newMessage.reduce(function(a, b) {
 
+			return a + " " + b;
+		});
+		return message2;
+	}
 })(Translator);
 
 
