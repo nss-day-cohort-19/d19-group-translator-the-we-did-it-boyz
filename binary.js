dbb2a9a600ca142b25dff0452e86ecfe880b7904
Nashville Software School <nashvillesoftwareschool@Nashvilles-MacBook-Pro.local>
2017-04-26 10:30:55 -0500

    working binary

--- binary.js
+++ binary.js
@@ -1,29 +1,23 @@
 console.log('binary loaded');
-var Translator= (function(){
-	var englishtoBi= {"merry":"01001101 01100101 01110010 01110010 01111001","christmas":"01100011 01101000 01110010 01101001 01110011 01110100 01101101 01100001 01110011","and":"01100001 01101110 01100100","happy":"01001000 01100001 01110000 01110000 01111001 ", "new":"01101110 01100101 01110111","year":"01111001 01100101 01100001 01110010"};
+var Binary= (function(Translator){
+	var englishtoBi= {"merry":"01001101011001010111001001110010 01111001","christmas":"01100011011010000111001001101001 0111001101110100011011010110000101110011","and":"011000010110111001100100","happy":"010010000110000101110000 0111000001111001 ", "new":"011011100110010101110111","year":"01111001011001010110000101110010"};
 	var binaryArray= Object.keys(englishtoBi).map(function(x){
-		return [Number(x), englishtoBi[x]];
+		return [String(x), englishtoBi[x]];
 	});
 	console.log(binaryArray);
-	return {
-		translateBinary: function(event){
-			var message=[""];
+		Translator.translateBinary= function(event){
+			var message=[];
 				for(word in event) {
 				for (var i = 0; i < binaryArray.length; i++) {
-					console.log(word, binaryArray[i]);
-					if(binaryArray[i][0] == word) {
-						message.push(word[i][1]);
-						console.log(word);
+					if(binaryArray[i][0] == event[word]) {
+						message.push(binaryArray[i][1]);
 						break;
 					}
 				}
 			}
-			console.log(message);
 			var message2 = message.reduce(function(a, b) {
-
 				return a + " " + b;
 			});
 			return message2;
 		}
-	}
-})();
+})(Translator);
--- german.js
+++ german.js
@@ -1,22 +1,22 @@
-var Translatorgerman = (function(){
+var Translator = (function(){
 	var germanwords = [["merry", "Frohe"], ["Chirstmas","Weihnachten"], ["and a", "und ein"], ["happy", "gutes"], ["new", "neues"], ["year", "Jahr"]];
 
 	return {
 		translateGerman: function(msg){
-			var translatedMsg = [];
+			var translatedMsg = [""];
 			for (x in msg){
 				for (var i = 0; i < germanwords.length; i++) {
 					console.log(x, germanwords[i]);
-					if(germanwords[i][0] == x){
-					 	translatedMsg.push(germanwords[i](0));
-						console.log("german words", x);
+					if(germanwords[i][0] == msg[x]){
+					 	translatedMsg.push(germanwords[i][1]);
+						break;
 					}
 				}
-			} console.log(translatedMsg);
+			}
 			  var germanTranslation = translatedMsg.reduce(function(a, b){
 			  	return a + " " + b;
 			  })
 			  	return germanTranslation;
 		}
 	}
-})();
+})(Translator);
