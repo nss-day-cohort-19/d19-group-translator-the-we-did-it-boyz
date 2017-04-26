2c7929a063a2c33bf1f81617eb2068db98173c95
Nashville Software School <nashvillesoftwareschool@Nashvilles-MacBook-Pro.local>
2017-04-26 11:24:25 -0500

    update to binary

--- binary.js
+++ binary.js
@@ -1,18 +1,19 @@
 console.log('binary loaded');
 var Binary= (function(Translator){
-	var englishtoBi= {"merry":"01001101011001010111001001110010 01111001","christmas":"01100011011010000111001001101001 0111001101110100011011010110000101110011","and":"011000010110111001100100","happy":"010010000110000101110000 0111000001111001 ", "new":"011011100110010101110111","year":"01111001011001010110000101110010"};
-	var binaryArray= Object.keys(englishtoBi).map(function(x){
-		return [String(x), englishtoBi[x]];
-	});
-	console.log(binaryArray);
+	var binObj= {"merry":"0100110101100101011100100111001001111001","christmas":"01100011011010000111001001101001 0111001101110100011011010110000101110011","and":"011000010110111001100100","happy":"010010000110000101110000 0111000001111001 ", "new":"011011100110010101110111","year":"01111001011001010110000101110010"};
 		Translator.translateBinary= function(event){
 			var message=[];
 				for(word in event) {
-				for (var i = 0; i < binaryArray.length; i++) {
-					if(binaryArray[i][0] == event[word]) {
-						message.push(binaryArray[i][1]);
+					var translate = false;
+					for(var key in binObj){
+					if(key == event[word]) {
+						message.push(binObj[key]);
+						translate= true;
 						break;
 					}
+				}
+				if(!translate){
+					message.push(event[word]);
 				}
 			}
 			var message2 = message.reduce(function(a, b) {
